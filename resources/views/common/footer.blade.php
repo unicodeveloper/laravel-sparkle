4316296617b78eb15d97996ed2ce5b12053da6d2
Taylor Otwell <taylorotwell@gmail.com>
2015-10-14 16:35:19 -0500

    working on design

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -9,17 +9,8 @@
             Copyright © {{ Spark::company() }} - <a href="/terms">Terms Of Service</a>
         </div>
 
-        <!-- Social Icons -->
         <div class="pull-right footer-social-icons">
-            <a href="http://facebook.com">
-                <i class="fa fa-btn fa-facebook-square"></i>
-            </a>
-            <a href="http://twitter.com">
-                <i class="fa fa-btn fa-twitter-square"></i>
-            </a>
-            <a href="http://github.com">
-                <i class="fa fa-github-square"></i>
-            </a>
+            &nbsp;
         </div>
 
         <div class="clearfix"></div>
