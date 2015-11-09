f5efa153da603a22e84828e4c1485dc4149d0c64
Buzdyk Gregory <buzdyk.gregory@gmail.com>
2015-11-09 18:18:47 +0300

    fix global libs override

--- resources/assets/js/core/dependencies.js
+++ resources/assets/js/core/dependencies.js
@@ -11,16 +11,17 @@ Vue.http.headers.common['X-CSRF-TOKEN'] = Spark.csrfToken;
 /*
  * Load Underscore.js, used for map / reduce on arrays.
  */
-window._ = require('underscore');
+if (window._ === undefined) window._ = require('underscore');
 
 /*
  * Load Moment.js, used for date formatting and presentation.
  */
-window.moment = require('moment');
+if (window.moment === undefined) window.moment = require('moment');
 
 /*
  * Load jQuery and Bootstrap jQuery, used for front-end interaction.
  */
-window.$ = window.jQuery = require('jquery');
+if (window.$ === undefined || window.jQuery === undefined) window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass/assets/javascripts/bootstrap');
+
 
