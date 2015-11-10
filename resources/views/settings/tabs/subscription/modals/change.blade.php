a62a6942168f01c82902f1c54eacbd7ddfae64dc
Taylor Otwell <taylorotwell@gmail.com>
2015-11-10 15:58:55 -0600

    working on some views

--- resources/views/settings/tabs/subscription/modals/change.blade.php
+++ resources/views/settings/tabs/subscription/modals/change.blade.php
@@ -10,7 +10,35 @@
 				<spark-errors :form="changePlanForm"></spark-errors>
 
 				<!-- Plan Selector -->
-				@include('spark::settings.tabs.subscription.modals.change.selector')
+				<div class="row">
+					<!-- Monthly / Available Plans -->
+					<div class="col-md-6">
+						<div class="spark-plan-change-selector-heading">
+							<span v-if="includesBothPlanIntervals">
+								<strong>Monthly Plans</strong>
+							</span>
+
+							<span v-else>
+								<strong>Available Plans</strong>
+							</span>
+						</div>
+
+						<div v-for="plan in defaultPlansExceptCurrent" style="margin-bottom: 10px;">
+							@include('spark::settings.tabs.subscription.modals.change.plan')
+						</div>
+					</div>
+
+					<!-- Yearly Plans -->
+					<div class="col-md-6" v-if="includesBothPlanIntervals">
+						<div class="spark-plan-change-selector-heading">
+							<strong>Yearly Plans</strong>
+						</div>
+
+						<div v-for="plan in yearlyPlansExceptCurrent" style="margin-bottom: 10px;">
+							@include('spark::settings.tabs.subscription.modals.change.plan')
+						</div>
+					</div>
+				</div>
 			</div>
 
 			<div class="modal-footer">
