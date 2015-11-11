90e602f0f1042295a1c9d829296cb5d905855c69
Taylor Otwell <taylorotwell@gmail.com>
2015-11-11 15:59:56 -0600

    add comment

--- resources/assets/js/forms/http.js
+++ resources/assets/js/forms/http.js
@@ -1,4 +1,7 @@
 module.exports = {
+    /**
+     * A few helper methods for making HTTP requests and doing common form actions.
+     */
     post: function (uri, form) {
         return Spark.sendForm('post', uri, form);
     },
